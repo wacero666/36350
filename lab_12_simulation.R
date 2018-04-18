a88e82b63ea0411ad949d179fad2ad9b0a98c3bb
wacero666 <whezhao@ucdavis.edu>
2018-04-18 11:00:31 -0400

    add model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,5 +1,27 @@
-generate_data(n, p) {
+generate_data = function(n, p) {
   covariates = matrix(rnorm(n*p, mean = 0, sd =1),n,p)
   responses = rnorm(n,mean = 0, sd=1)
   return(list(covariates = covariates, responses = responses))
 }
+
+model_select = function(covariates, responses,cutoff) {
+  v = c()
+  for(i in 1:nrow(covariates)){
+    if(summary(lm(responses~covariates[,i]))$coefficients[,4][2]<=cutoff){
+      v = c(v, i)
+    }
+  }
+  if(is.null(v)) {
+    return(c())
+  }
+  else {
+    return(summary(lm(responses~covariates[,v]))$coefficients[,4])
+  }
+}
+
+
+
+
+
+
+
